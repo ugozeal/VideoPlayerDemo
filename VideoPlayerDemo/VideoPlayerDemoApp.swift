e5089cba6642066720f26273e16514a512b9d94a
David Okonkwo <okonkwodavidu@gmail.com>
2023-03-23 09:10:07 +0100

    Initial Commit

---
+++ VideoPlayerDemo/VideoPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerDemoApp.swift
+//  VideoPlayerDemo
+//
+//  Created by David Okonkwo on 23/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
